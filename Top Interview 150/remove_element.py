a539ee4b60936f3d3f1a16d6ffa66f94d4b4df6f
prathameshbhavsar19 <prathameshbhavsar19@gmail.com>
2025-04-06 18:36:59 -0500

    Problem 2 added

---
+++ Top Interview 150/remove_element.py
@@ -0,0 +1,18 @@
+def removeElement(nums, val):
+    i, k = 0, 0
+    while i <= len(nums)-1:
+        if nums[i] != val:
+            nums[k] = nums[i]
+            k = k + 1
+        i = i + 1
+    return k
+
+nums = [1, 0, 0, 2, 5, 6, 8, 9]
+val = 0
+
+size = removeElement(nums, val)
+
+print(f"Size = {size}")
+print("nums = ")
+for n in nums:
+    print(f"{n}", end=" ")
